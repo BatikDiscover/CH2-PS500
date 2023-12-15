142b1baf8c19c9dd7ffcb3b22b04ac6a691553be
Akbar Hidayat <119398433+akbarabe@users.noreply.github.com>
2023-12-15 15:04:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const article = require("./api/article");
 const event = require("./api/event");
 
 // plugin model
-const model = require("./api/model");
+const model = require("./api/ml-connector");
 
 const init = async () => {
   const server = Hapi.server({
