00e1de6977dbfbd38049486eba54ab52b7d9b1c7
Akbar Hidayat <119398433+akbarabe@users.noreply.github.com>
2023-12-17 21:01:51 +0700

    Update handler.js
    Memperbaiki iterasi pada if

--- api/ml-connector/handler.js
+++ api/ml-connector/handler.js
@@ -1,9 +1,6 @@
 const { Firestore } = require("../../config/firebase");
 const autoBind = require("auto-bind");
-const { loadModel, predict } = require("./ml");
-
-// Inisialisasi Firestore client
-const firestore = new Firestore();
+const { predict } = require("./ml");
 
 class ModelHandler {
   constructor(validator) {
@@ -12,162 +9,167 @@ class ModelHandler {
   }
 
   async predictsHandler(request, h) {
-    const model = await loadModel();
+    // const model = await loadModel();
     console.log("model loaded!");
     try {
       // Mendapatkan hasil prediksi dari model
       const { image } = request.payload;
-      const predictions = await predict(model, image);
+      const predictions = await predict(image);
       console.log(predictions);
       const [Celup, Insang, Kawung, Megamendung, Parang, Poleng, Truntum] =
         predictions;
 
-      if (Celup) {
-        // Jika prediksi adalah "celup", maka ambil informasi dari Firestore
-        const docRef = firestore.collection("BatikIndonesia").doc("BatikCelup");
-        const docSnapshot = await docRef.get();
+      if (predictions) {
+        if (Celup) {
+          // Jika prediksi adalah "celup", maka ambil informasi dari Firestore
+          const docRef =
+            Firestore.collection("BatikIndonesia").doc("BatikCelup");
+          const docSnapshot = await docRef.get();
 
-        if (!docSnapshot.exists) {
-          console.log("Dokumen tidak ditemukan!");
-          return { result: "defaultResult" };
+          if (!docSnapshot.exists) {
+            console.log("Dokumen tidak ditemukan!");
+            return { result: "defaultResult" };
+          }
+
+          // Mengambil data dari Firestore
+          const celupData = docSnapshot.data();
+
+          // Mengembalikan informasi dari item "BatikCelup"
+          return {
+            Nama: celupData.Nama,
+            Asal: celupData.Asal,
+            Pola: celupData.Pola,
+            Makna: celupData.Makna,
+          };
         }
 
-        // Mengambil data dari Firestore
-        const celupData = docSnapshot.data();
+        if (Insang) {
+          const docRef = firestore
+            .collection("BatikIndonesia")
+            .doc("BatikInsang");
+          const docSnapshot = await docRef.get();
 
-        // Mengembalikan informasi dari item "BatikCelup"
-        return {
-          Nama: celupData.Nama,
-          Asal: celupData.Asal,
-          Pola: celupData.Pola,
-          Makna: celupData.Makna,
-        };
-      }
+          if (!docSnapshot.exists) {
+            console.log("Dokumen tidak ditemukan!");
+            return { result: "defaultResult" };
+          }
 
-      if (Insang) {
-        const docRef = firestore
-          .collection("BatikIndonesia")
-          .doc("BatikInsang");
-        const docSnapshot = await docRef.get();
+          const insangData = docSnapshot.data();
 
-        if (!docSnapshot.exists) {
-          console.log("Dokumen tidak ditemukan!");
-          return { result: "defaultResult" };
+          return {
+            Nama: insangData.Nama,
+            Asal: insangData.Asal,
+            Pola: insangData.Pola,
+            Makna: insangData.Makna,
+          };
         }
 
-        const insangData = docSnapshot.data();
+        if (Kawung) {
+          const docRef = firestore
+            .collection("BatikIndonesia")
+            .doc("BatikKawung");
+          const docSnapshot = await docRef.get();
 
-        return {
-          Nama: insangData.Nama,
-          Asal: insangData.Asal,
-          Pola: insangData.Pola,
-          Makna: insangData.Makna,
-        };
-      }
+          if (!docSnapshot.exists) {
+            console.log("Dokumen tidak ditemukan!");
+            return { result: "defaultResult" };
+          }
 
-      if (Kawung) {
-        const docRef = firestore
-          .collection("BatikIndonesia")
-          .doc("BatikKawung");
-        const docSnapshot = await docRef.get();
+          const kawungData = docSnapshot.data();
 
-        if (!docSnapshot.exists) {
-          console.log("Dokumen tidak ditemukan!");
-          return { result: "defaultResult" };
+          return {
+            Nama: kawungData.Nama,
+            Asal: kawungData.Asal,
+            Pola: kawungData.Pola,
+            Makna: kawungData.Makna,
+          };
         }
 
-        const kawungData = docSnapshot.data();
+        if (Megamendung) {
+          const docRef = firestore
+            .collection("BatikIndonesia")
+            .doc("BatikMegamendung");
+          const docSnapshot = await docRef.get();
 
-        return {
-          Nama: kawungData.Nama,
-          Asal: kawungData.Asal,
-          Pola: kawungData.Pola,
-          Makna: kawungData.Makna,
-        };
-      }
+          if (!docSnapshot.exists) {
+            console.log("Dokumen tidak ditemukan!");
+            return { result: "defaultResult" };
+          }
 
-      if (Megamendung) {
-        const docRef = firestore
-          .collection("BatikIndonesia")
-          .doc("BatikMegamendung");
-        const docSnapshot = await docRef.get();
+          const megamendungData = docSnapshot.data();
 
-        if (!docSnapshot.exists) {
-          console.log("Dokumen tidak ditemukan!");
-          return { result: "defaultResult" };
+          return {
+            Nama: megamendungData.Nama,
+            Asal: megamendungData.Asal,
+            Pola: megamendungData.Pola,
+            Makna: megamendungData.Makna,
+          };
         }
 
-        const megamendungData = docSnapshot.data();
+        if (Parang) {
+          const docRef = firestore
+            .collection("BatikIndonesia")
+            .doc("BatikParang");
+          const docSnapshot = await docRef.get();
 
-        return {
-          Nama: megamendungData.Nama,
-          Asal: megamendungData.Asal,
-          Pola: megamendungData.Pola,
-          Makna: megamendungData.Makna,
-        };
-      }
+          if (!docSnapshot.exists) {
+            console.log("Dokumen tidak ditemukan!");
+            return { result: "defaultResult" };
+          }
 
-      if (Parang) {
-        const docRef = firestore
-          .collection("BatikIndonesia")
-          .doc("BatikParang");
-        const docSnapshot = await docRef.get();
+          const parangData = docSnapshot.data();
 
-        if (!docSnapshot.exists) {
-          console.log("Dokumen tidak ditemukan!");
-          return { result: "defaultResult" };
+          return {
+            Nama: parangData.Nama,
+            Asal: parangData.Asal,
+            Pola: parangData.Pola,
+            Makna: parangData.Makna,
+          };
         }
 
-        const parangData = docSnapshot.data();
+        if (Poleng) {
+          const docRef = firestore
+            .collection("BatikIndonesia")
+            .doc("BatikPoleng");
+          const docSnapshot = await docRef.get();
 
-        return {
-          Nama: parangData.Nama,
-          Asal: parangData.Asal,
-          Pola: parangData.Pola,
-          Makna: parangData.Makna,
-        };
-      }
+          if (!docSnapshot.exists) {
+            console.log("Dokumen tidak ditemukan!");
+            return { result: "defaultResult" };
+          }
 
-      if (Poleng) {
-        const docRef = firestore
-          .collection("BatikIndonesia")
-          .doc("BatikPoleng");
-        const docSnapshot = await docRef.get();
+          const polengData = docSnapshot.data();
 
-        if (!docSnapshot.exists) {
-          console.log("Dokumen tidak ditemukan!");
-          return { result: "defaultResult" };
+          return {
+            Nama: polengData.Nama,
+            Asal: polengData.Asal,
+            Pola: polengData.Pola,
+            Makna: polengData.Makna,
+          };
         }
 
-        const polengData = docSnapshot.data();
+        if (Truntum) {
+          const docRef = firestore
+            .collection("BatikIndonesia")
+            .doc("BatikTruntum");
+          const docSnapshot = await docRef.get();
 
-        return {
-          Nama: polengData.Nama,
-          Asal: polengData.Asal,
-          Pola: polengData.Pola,
-          Makna: polengData.Makna,
-        };
-      }
+          if (!docSnapshot.exists) {
+            console.log("Dokumen tidak ditemukan!");
+            return { result: "defaultResult" };
+          }
 
-      if (Truntum) {
-        const docRef = firestore
-          .collection("BatikIndonesia")
-          .doc("BatikTruntum");
-        const docSnapshot = await docRef.get();
+          const truntumData = docSnapshot.data();
 
-        if (!docSnapshot.exists) {
-          console.log("Dokumen tidak ditemukan!");
-          return { result: "defaultResult" };
+          return {
+            Nama: truntumData.Nama,
+            Asal: truntumData.Asal,
+            Pola: truntumData.Pola,
+            Makna: truntumData.Makna,
+          };
         }
-
-        const truntumData = docSnapshot.data();
-
-        return {
-          Nama: truntumData.Nama,
-          Asal: truntumData.Asal,
-          Pola: truntumData.Pola,
-          Makna: truntumData.Makna,
-        };
+      } else {
+        return { result: "Batik tidak terdeteksi" };
       }
     } catch (error) {
       console.error("Error accessing Firestore:", error);
