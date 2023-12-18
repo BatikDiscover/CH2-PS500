eb1d57c9a1d2cd11828a4e205f3a4c4e05b670c5
romijatmiko <romijatmiko033@gmail.com>
2023-12-19 02:15:49 +0700

    update ui

--- app/src/main/java/com/bangkit/batikdiscover/ui/product/ProductDetailFragment.kt
+++ app/src/main/java/com/bangkit/batikdiscover/ui/product/ProductDetailFragment.kt
@@ -11,4 +11,28 @@ import com.bangkit.batikdiscover.R  // Make sure to replace this with the actual
 
 class ProductDetailFragment : Fragment() {
 
+    private lateinit var imageView8: ImageView
+    private lateinit var textView10: TextView
+    private lateinit var textView123: TextView
+    private lateinit var textView11: TextView
+    private lateinit var textView12: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_product_detail, container, false)
+
+        // Bind views
+        imageView8 = view.findViewById(R.id.imageView8)
+        textView10 = view.findViewById(R.id.textView10)
+        textView123 = view.findViewById(R.id.textVIew123)
+        textView11 = view.findViewById(R.id.textView11)
+        textView12 = view.findViewById(R.id.textView12)
+
+        // Now you can use these variables to manipulate the views
+
+        return view
+    }
 }
